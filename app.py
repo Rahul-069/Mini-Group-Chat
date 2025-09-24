165824991fd164c4d9c3bd0980a88949901d9f0b
Rahul-069 <136224663+Rahul-069@users.noreply.github.com>
2025-09-24 23:35:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,6 +143,7 @@ def handle_disconnect():
 #     # local dev (optional): insecure, runs Flask dev server
 #     socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False, allow_unsafe_werkzeug=True)
+
 
 
