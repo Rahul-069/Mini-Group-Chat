2a5c5187c98d77d1417606e890ace149959d7ecf
Rahul-069 <136224663+Rahul-069@users.noreply.github.com>
2025-09-24 23:24:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,11 @@ def handle_disconnect():
             save_users(users)
         emit('system', f'{username} disconnected', broadcast=True)
 
-# Note: In Render we will use Gunicorn to run the app.
+# # Note: In Render we will use Gunicorn to run the app.
+# if __name__ == "__main__":
+#     # local dev (optional): insecure, runs Flask dev server
+#     socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
 if __name__ == "__main__":
-    # local dev (optional): insecure, runs Flask dev server
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
 
